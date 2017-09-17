36f1d387fc94390f1e49237779d5ff3ae49211db
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2017-09-17 01:11:39 -0300

    Implementación del punto 1

---
+++ src/Ascensor.java
@@ -0,0 +1,69 @@
+
+class Ascensor {
+
+    private int pisoActual;
+    private int ultimoPiso;
+    
+    /**
+     * pre : 'cantidadDePisos' está en el rango [1..141]
+     * post: el ascensor está en planta baja (piso 0), con la
+     *       posibilidad de moverse entre 0 y 'cantidadDePisos'.
+     * 
+     */
+    public Ascensor(int cantidadDePisos) {
+        
+        pisoActual = 0;
+        
+        if (cantidadDePisos > 0 && cantidadDePisos <= 141) {
+            
+            ultimoPiso = cantidadDePisos;
+            
+        } else {
+        
+            ultimoPiso = 141;
+        }
+    }
+    
+    /**
+     * post: devuelve el número de piso en el que se
+     *       encuentra el ascensor, 0 si está en planta baja.
+     */
+    public int obtenerPisoActual() {
+        
+        return pisoActual;
+    }
+    
+    /**
+     * post: devuelve el último piso al que puede moverse
+     *       el ascensor.
+     */
+    public int obtenerUltimoPiso() {
+        
+        return ultimoPiso;
+    }
+    
+    /**
+     * pre : 'pisoAlQueIr' está en el rango [0, 'obtenerUltimoPiso()'].
+     * post: mueve el ascensor al piso indicado y devuelve la cantidad
+     *       de pisos por los que se desplazó.
+     */
+    public int irAlPiso(int pisoAlQueIr) {
+        
+        int desplazamiento = 0;
+        
+        if ((pisoAlQueIr >=0) && (pisoAlQueIr <= ultimoPiso)) {
+            
+            if (pisoAlQueIr > pisoActual) {
+                
+                desplazamiento = pisoAlQueIr - pisoActual;
+
+            } else {
+                
+                desplazamiento = pisoActual - pisoAlQueIr;
+            }
+            pisoActual = pisoAlQueIr;
+        }
+        
+        return desplazamiento;
+    }
+}
